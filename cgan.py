b4f07487f0c211734bf5575d40c4d53ecdc4dedd
Aryan Jain <jainaryan1310@gmail.com>
2022-03-15 17:35:47 +0530

    cifar10 cgan

--- cgan.py
+++ cgan.py
@@ -13,7 +13,7 @@ import torchvision.utils as vutils
 
 
 parser = argparse.ArgumentParser()
-#parser.add_argument('--dataset', required=True, help='cifar10 | lsun | mnist')
+parser.add_argument('--dataset', required=True, help='cifar10 | lsun | mnist')
 parser.add_argument('--dataroot', required=True, help='path to data')
 parser.add_argument('--batchSize', type=int, default=64, help='input batch size')
 parser.add_argument('--imageSize', type=int, default=32, help='image size input')
